79176934050ba05018bd3723a39b36cf0010d27d
Arunkaash <arunkumarkr101@gmail.com>
2024-05-25 16:17:32 +0530

    employeeApp

--- src/main/java/com/EmProjectApplication/repository/EmployeeRepository.java
+++ src/main/java/com/EmProjectApplication/repository/EmployeeRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> { //interface that extends JpaRepository. This interface will provide CRUD operations (using to save data in the table)
 //JpaRepository is a special type of library that helps us store and manage data in a database.
+
 }
